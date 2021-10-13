957c0c5f97c0ca1530d3fc2624921a1a232875fc
bam <juhyun19981129@gmail.com>
2021-10-13 18:50:06 +0900

    기본 뼈대및 폰트 css 추가

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let score;
 
     document.body.onclick = function (event) {
-        score = document.getElementById('score_view');
+        score = document.getElementById('score');
         score.textContent = Number(score.textContent) + 1;
 
         console.log(score);
