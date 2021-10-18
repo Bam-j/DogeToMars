6108c043297de1fd4b28fab14ad7efcac8a572cb
bam <juhyun19981129@gmail.com>
2021-10-18 17:17:32 +0900

    터치 이벤트 추가2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     let scoreViewText = document.getElementById('score');
     let score;
+    let lastTouchEndTime = 0;
 
     //우클릭 방지 이벤트
     document.addEventListener('contextmenu', function (event) {
         return event.preventDefault();
-    });
+    }, false);
 
     //로컬스토리지에 정보가 있다면, 저장된 클릭 횟수 부터 표시하고 카운트
     if(JSON.parse(localStorage.getItem('score')) !== 0) {
@@ -25,14 +26,22 @@ document.addEventListener('DOMContentLoaded', function () {
         score += 1;
 
         localStorage.setItem('score', JSON.stringify(score));
-    });
+    }, false);
 
 
-    document.body.addEventListener('touchend', function () {
+    //터치 이벤트
+    document.body.addEventListener('touchend', function (event) {
+        let currentTouchEndTime = (newDate()).getTime();
+
+        if ( currentTouchEndTime - lastTouchEndTime <= 300 ) {
+            event.preventDefault();
+        }
+        lastTouchEndTime = currentTouchEndTime;
+
         scoreViewText.textContent = Number(scoreViewText.textContent) + 1;
 
         score += 1;
 
         localStorage.setItem('score', JSON.stringify(score));
-    })
-});
+    }, false);
+}, false);
