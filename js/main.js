882cd22956117027f42aabae26957784b46514be
bam <juhyun19981129@gmail.com>
2021-10-18 17:22:49 +0900

    두번 탭 터치 확대기능 제거1

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }, false);
 
     //로컬스토리지에 정보가 있다면, 저장된 클릭 횟수 부터 표시하고 카운트
-    if(JSON.parse(localStorage.getItem('score')) !== 0) {
+    if (JSON.parse(localStorage.getItem('score')) !== 0) {
         score = Number(JSON.parse(localStorage.getItem('score')));
         scoreViewText.textContent = JSON.parse(localStorage.getItem('score'));
     }
@@ -31,17 +31,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //터치 이벤트
     document.body.addEventListener('touchend', function (event) {
-        let currentTouchEndTime = (newDate()).getTime();
-
-        if ( currentTouchEndTime - lastTouchEndTime <= 300 ) {
-            event.preventDefault();
-        }
-        lastTouchEndTime = currentTouchEndTime;
-
         scoreViewText.textContent = Number(scoreViewText.textContent) + 1;
 
         score += 1;
 
         localStorage.setItem('score', JSON.stringify(score));
     }, false);
+
+    document.addEventListener('touchend', function (event) {
+        let currentTouchEndTime = (newDate()).getTime();
+
+        if (currentTouchEndTime - lastTouchEndTime <= 300) {
+            event.preventDefault();
+        }
+        lastTouchEndTime = currentTouchEndTime;
+    }, false);
+
 }, false);
