6cdc0c636d6e57be6b87d97377522351eb1ce029
bam <juhyun19981129@gmail.com>
2021-10-26 14:45:35 +0900

    문제점 추가

--- js/slide-view.js
+++ js/slide-view.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const slideMenu = document.getElementById('slide_ranking_view');
     const closeMenuBtn = document.getElementById('close_btn');
 
+    //랭킹을 표시할 슬라이드 서랍 메뉴 만들기
     slideBtn.addEventListener('click', function() {
         slideBtn.style['display'] = 'none';
         slideMenu.style['transform'] = 'translate(0px, 0px)';
