38aeb1015b89a005c69804955335ce13b5a03db5
bam <juhyun19981129@gmail.com>
2021-10-28 15:54:21 +0900

    index.html 구조 수정

--- js/touch-events.js
+++ js/touch-events.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let scoreViewText = document.getElementById('score');
+    let scoreView = document.getElementById('score');
     let score;
     let lastTouchEndTime = 0;
 
@@ -11,17 +11,17 @@ document.addEventListener('DOMContentLoaded', function () {
     //로컬스토리지에 정보가 있다면, 저장된 클릭 횟수 부터 표시하고 카운트
     if (JSON.parse(localStorage.getItem('score')) !== 0) {
         score = Number(JSON.parse(localStorage.getItem('score')));
-        scoreViewText.textContent = JSON.parse(localStorage.getItem('score'));
+        scoreView.textContent = JSON.parse(localStorage.getItem('score'));
     }
     //새로운 접속이면 0부터 시작
     else {
         score = 0;
-        scoreViewText.textContent = 0;
+        scoreView.textContent = 0;
     }
 
     //클릭 했을 경우, 클릭수 + 1 이벤트
     document.body.addEventListener('click', function () {
-        scoreViewText.textContent = Number(scoreViewText.textContent) + 1;
+        scoreView.textContent = Number(scoreView.textContent) + 1;
 
         score += 1;
 
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //줌 확대 기능 방지: touch 갯수가 2개 이상이면 터치 이벤트 무효화
     document.documentElement.addEventListener('touchstart', function (event) {
-        if(event.touches.length > 1) {
+        if (event.touches.length > 1) {
             event.preventDefault();
         }
     }, false);
