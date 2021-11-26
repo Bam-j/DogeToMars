277ee431b470b30942340ee6fd8e2cc7598fd5be
bam <juhyun19981129@gmail.com>
2021-11-27 02:10:14 +0900

    최종완성 2

---
+++ js/db-connect.js
@@ -0,0 +1,33 @@
+export const mariaDBConn = () => {
+    const mariadb = require('mariadb');
+    const pool = mariadb.createPool({
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        database: 'score_board',
+    });
+    const sql = 'select sco from score_board';
+
+    async function GetScoreList() {
+        let conn;
+        let rows;
+
+        try {
+            conn = await pool.getConnection();
+            rows = await conn.query(sql);
+        }
+        catch (e) {
+            throw e;
+        }
+        finally {
+            if (conn) {
+                await conn.end();
+            }
+            return rows;
+        }
+        return rows;
+    }
+    module.exports = {
+        getScoreList: GetScoreList
+    }
+};
---
+++ js/high-score.js
@@ -0,0 +1,20 @@
+const mariaDBConn = require('./db-connect.js');
+
+document.addEventListener('DOMContentLoaded', function () {
+    mariaDBConn.getScoreList()
+        .then(rows => {
+            let scoreArr = rows.trim().split('');
+
+            for (let i = 0; i < scoreArr.length; i++) {
+                scoreArr[i] = parseInt(scoreArr[i]);
+            }
+
+            const highScore = Math.max(...scoreArr);
+            const highScoreView = document.getElementById('high-score');
+
+            highScoreView.textContent = highScore.toString().trim();
+        })
+        .catch(e => {
+            return console.error(e);
+        });
+}, false);
